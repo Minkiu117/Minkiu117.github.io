489117304f8ba9a565772e81f19144d57cd8d432
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-26 02:00:26 -0500

    Update robots.js

--- rv/robots.js
+++ rv/robots.js
@@ -1,196 +1,56 @@
-function Sensor(position,direction){
- THREE.Raycaster.call(this,position,direction);
- this.colision=false;
-}
-Sensor.prototype=new THREE.Raycaster();
+function setup() {
+  
+  var esferaForma = new THREE.SphereGeometry(2,100,100);
+  var mediaesferaForma = new THREE.SphereGeometry( 1.15, 100, 50, 0, Math.PI*2, 3*Math.PI/2, Math.PI);
+  var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
+  var cilindroForma2 = new THREE.CylinderGeometry(0.02,0.02,0.5,100);
+  var cilindroForma3 = new THREE.CylinderGeometry(1.15,1,0.2,100);
+  
+  var esfera1 = new THREE.Mesh(esferaForma);
+  var mediaesfera1 = new THREE.Mesh(mediaesferaForma);
+  var cilindromalla = new THREE.Mesh( cilindroForma);
+  var cilindromalla2 = new THREE.Mesh( cilindroForma2);
+  var cilindromalla3 = new THREE.Mesh (cilindroForma3);
+  
+  cilindromalla.position.y=3.85;
+  cilindromalla.position.x=0.13;
+  cilindromalla2.position.y=3.6;
+  cilindromalla2.position.x=-0.15;
+  cilindromalla3.position.y=2.2;
+  mediaesfera1.position.y=2.35;
+  
 
-function BB8Cabeza(){
- THREE.Object3D.call(this);
- THREE.ImageUtils.crossOrigin = '';
- var texturahead = THREE.ImageUtils.loadTexture('http://francisgoba.github.io/rv/bb8head2.jpg');
- this.cabeza=new THREE.Mesh(new THREE.SphereGeometry    (1.15,100,50,0,Math.PI*2,3*Math.PI/2,Math.PI),new THREE.MeshPhongMaterial({map:texturahead}));
- this.cuello=new THREE.Mesh(new THREE.CylinderGeometry(1.15,1,0.2,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.antena1=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.antena2=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.5,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.cabeza.position.y=1.95;
- this.antena1.position.y=3.35;
- this.antena1.position.x=0.13;
- this.antena2.position.y=3.1;
- this.antena2.position.x=-0.15;
- this.cuello.position.y=1.85;
- this.add(this.cabeza);
- this.add(this.antena1);
- this.add(this.antena2);
- this.add(this.cuello);
-}
-
-BB8Cabeza.prototype=new THREE.Object3D();
-
-function BB8(x=0, y=0){
- Agent.call(this,x,y);
- THREE.ImageUtils.crossOrigin = '';
- var textura = THREE.ImageUtils.loadTexture('http://francisgoba.github.io/rv/bb8body.jpg');
- this.cuerpo=new THREE.Mesh(new THREE.SphereGeometry(2,100,100), new THREE.MeshPhongMaterial ({map:textura}));
- this.cabezabb8 = new BB8Cabeza();
- this.cuerpo.rotation.z=-0.25;  
- this.add(this.cuerpo);
- this.add(this.cabezabb8);
-
- this.sensor=new Sensor();
- //this.sensor2=new Sensor();
- this.actuator=new Array();
- 
- this.cuerpo.rotation.x=Math.PI/2;
- this.cabezabb8.rotation.x=Math.PI/2;
- this.cabezabb8.rotation.y=Math.PI+0.5;
- this.cuerpo.scale.x=0.5;
- this.cuerpo.scale.y=0.5;
- this.cuerpo.scale.z=0.5;
- this.cabezabb8.scale.x=0.5;
- this.cabezabb8.scale.y=0.5;
- this.cabezabb8.scale.z=0.5;
-}
-BB8.prototype=new Agent();
-
-function Wall(size,x=0,y=0){
- THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size), new THREE.MeshNormalMaterial()); 
- this.size=size;
- this.position.x=x;
- this.position.y=y;
-}
-Wall.prototype=new THREE.Mesh();
-
-Environment.prototype.setMap=function(map){
- var offset=Math.floor(map.length/2);
- for(var i=0;i<map.length;i++){
-  for(var j=0;j<map.length;j++){
-   if(map[i][j]==="x")
-    this.add(new Wall(1, j-offset,-(i-offset)));
-   else if(map[i][j]==="r")
-    this.add(new BB8(j-offset,-(i-offset)));
-  }
- }
-}	
-
-BB8.prototype.sense=function(environment){
- this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
- //this.sensor2.set(this.position, new THREE.Vector3(Math.sin(this.rotation.z),Math.cos(this.rotation.z),0));
- var obstaculo = this.sensor.intersectObjects(environment.children,true);
- //var obstaculo2 = this.sensor2.intersectObjects(environment.children,true);
- if ((obstaculo.length>0&&(obstaculo[0].distance<=1)))
-  this.sensor.colision=true;
- else
-  this.sensor.colision=false;
- /*if((obstaculo2.length>0&&(obstaculo2[0].distance<=1)))
-  this.sensor2.colision=true;
- else
-  this.sensor2.colision=false;*/
+  var forma = new THREE.Geometry();
+  
+  THREE.GeometryUtils.merge(forma, esfera1);
+  THREE.GeometryUtils.merge(forma, mediaesfera1);
+  THREE.GeometryUtils.merge(forma, cilindromalla);
+  THREE.GeometryUtils.merge(forma, cilindromalla2);
+  THREE.GeometryUtils.merge(forma, cilindromalla3);
+  
+  malla = new THREE.Mesh( forma );
+  
+  escena = new THREE.Scene();
+  escena.add( malla );
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 10;
+  
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95);
+  document.body.appendChild( renderer.domElement );
 }
 
-BB8.prototype.plan = function(environment){
- this.actuator.commands=[];
- /*if(this.sensor.colision==false && this.sensor2.colision==true)
-  this.actuator.commands.push('Derecho');
- else if(this.sensor.colision==true && this.sensor2.colision==true)
-   this.actuator.commands.push('RotarDerecha');
- else
-   this.actuator.commands.push('RotarIzquierda');*/
-  if(this.sensor.colision==true)
-   this.actuator.commands.push('RotarIzquierda');
-  else
+function loop() {
+  requestAnimationFrame( loop );
+  
+  //malla.rotation.x += 0.01;
+  malla.rotation.y += 0.01;
+  
+  renderer.render( escena, camara);
 }
 
-BB8.prototype.act=function(environment){
- var command=this.actuator.commands.pop();
- if(command==undefined)
-  console.log('Undefined command');
- else if(command in this.operations)
-  this.operations[command](this);
- else
-  console.log('Unknown command'); 
-}
-
-BB8.prototype.operations = {};
-
-BB8.prototype.operations.Derecho = function(robot,step){
- if(step==undefined)
-  step=0.1;
- robot.position.x+=step*Math.cos(robot.rotation.z);
- robot.position.y+=step*Math.sin(robot.rotation.z);
- robot.cuerpo.rotation.z-=0.5;
-};
-
-BB8.prototype.operations.RotarDerecha = function(robot,angulo){
- if(angulo==undefined){
-  angulo=-Math.PI/2;
- }
- robot.rotation.z+=angulo;
-};
-
-BB8.prototype.operations.RotarIzquierda = function(robot,angulo){
- if(angulo==undefined){
-  angulo=Math.PI/2;
- }
- robot.rotation.z+=angulo;
-};
- 
-function setup(){
- var mapa = new Array();
-  mapa[0] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-  mapa[1] = "x   r                      x";
-  mapa[2] = "x                          x";
-  mapa[3] = "x                          x";
-  mapa[4] = "x                          x";
-  mapa[5] = "x                          x";
-  mapa[6] = "xxxxxxxxxxxxxxxx    xxxxxxxx";
-  mapa[7] = "x                          x";
-  mapa[8] = "x                          x";
-  mapa[9] = "x                          x";
- mapa[10] = "x                          x";
- mapa[11] = "x                          x";
- mapa[12] = "x                          x";
- mapa[13] = "x                          x";
- mapa[14] = "x                          x";
- mapa[15] = "x                          x";
- mapa[16] = "x                          x";
- mapa[17] = "x                          x";
- mapa[18] = "x                          x";
- mapa[19] = "x     r                    x";
- mapa[20] = "x                          x";
- mapa[21] = "x                          x";
- mapa[22] = "x                          x";
- mapa[23] = "x                          x";
- mapa[24] = "xxxxxxxxxxxxx           xxxx";
- mapa[25] = "x     r                    x";
- mapa[26] = "x                          x";
- mapa[27] = "x                          x";
- mapa[28] = "x                          x";
- mapa[29] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
- entorno=new Environment();
- entorno.setMap(mapa);
- luzPuntual = new THREE.PointLight(0xffffff);
- luzPuntual.position.x=0;  
- luzPuntual.position.y=10;
- luzPuntual.position.z=30;
- camara=new THREE.PerspectiveCamera();
- camara.position.z=50;
- renderer = new THREE.WebGLRenderer();
- renderer.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
- document.body.appendChild(renderer.domElement);
- entorno.add(camara);
- entorno.add(luzPuntual);
-}
-
-function loop(){
- requestAnimationFrame(loop);
- entorno.sense();
- entorno.plan();
- entorno.act();
- renderer.render(entorno,camara);
-}
-
-var entorno,luzPuntual,robot,step,angulo,camara,renderer;
+var escena, camara, render,malla;
 
 setup();
 loop();
