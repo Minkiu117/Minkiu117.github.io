8fabb1aa733b8008f2f894a8515e8373408a2c6c
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-28 03:01:32 -0500

    Update robots.js

--- rv/robots.js
+++ rv/robots.js
@@ -1,28 +1,28 @@
 function setup() {
   
-  var esferaForma = new THREE.SphereGeometry(1.5, 100, 100, 0, Math.PI*2, 1*Math.PI/4, Math.PI/2);
-  var mediaesferaForma = new THREE.SphereGeometry( 1.5, 100, 100, 0, Math.PI*2, 3*Math.PI/4, Math.PI/2);
+  var esferaFormas = new THREE.SphereGeometry(1.5, 100, 100, 0, Math.PI*2, 1*Math.PI/2, Math.PI/2);
+  var esferaFormai = new THREE.SphereGeometry(1.5, 100, 100, 0, Math.PI*2, 1*Math.PI/2, Math.PI/2);
+  var mediaesferaForma = new THREE.SphereGeometry( 1.5, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI/2);
   var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
   var cilindroForma2 = new THREE.CylinderGeometry(0.02,0.02,0.5,100);
-  var cilindroForma3 = new THREE.CylinderGeometry(1.15,1,0.2,100);
   
   var esfera1 = new THREE.Mesh(esferaForma);
+  var esfera2 = new THREE.Mesh(esferaForma);
   var mediaesfera1 = new THREE.Mesh(mediaesferaForma);
   var cilindromalla = new THREE.Mesh( cilindroForma);
   var cilindromalla2 = new THREE.Mesh( cilindroForma2);
-  var cilindromalla3 = new THREE.Mesh (cilindroForma3);
   
   cilindromalla.position.y=3.85;
   cilindromalla.position.x=0.13;
   cilindromalla2.position.y=3.6;
   cilindromalla2.position.x=-0.15;
-  cilindromalla3.position.y=2.2;
   mediaesfera1.position.y=2.35;
   
 
   var forma = new THREE.Geometry();
   
   THREE.GeometryUtils.merge(forma, esfera1);
+  THREE.GeometryUtils.merge(forma, esfera2);
   THREE.GeometryUtils.merge(forma, mediaesfera1);
   THREE.GeometryUtils.merge(forma, cilindromalla);
   THREE.GeometryUtils.merge(forma, cilindromalla2);
