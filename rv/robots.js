ba73e581db02088fbe4be36a7a29d2ecdce2d90f
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-28 04:06:13 -0500

    Update robots.js

--- rv/robots.js
+++ rv/robots.js
@@ -1,7 +1,7 @@
 function setup() {
   
-  var esferaFormai = new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .55);
-  var esferaFormas = new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .675);
+  var esferaFormai = new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56);
+  var esferaFormas = new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68);
   var mediaesferaForma = new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI/2);
   var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
   
