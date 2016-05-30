0cf2d509677e55d33780dabe225cd46ba8f06432
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 04:23:15 -0500

    Update robottexturas.js

--- rv/robottexturas.js
+++ rv/robottexturas.js
@@ -42,7 +42,6 @@ function loop(){
   Ovnibot.cuerpos.rotation.y-=0.01;
   Ovnibot.cuerpoi.rotation.y-=0.01;
   Ovnibot.rotation.y+=0.01;
-  Ovnibot.rotation.x+=0.01;
 }
 
 var escena,camara,luzPuntual,renderer;
