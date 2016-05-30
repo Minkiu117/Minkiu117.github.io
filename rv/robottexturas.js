c535cba11f5a3a3322ba5b90cec4bf924835d909
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 03:36:02 -0500

    Update robottexturas.js

--- rv/robottexturas.js
+++ rv/robottexturas.js
@@ -1,39 +1,31 @@
-function Casco(){
+function Ovni(){
   THREE.Object3D.call(this);
+  THREE.ImageUtils.crossOrigin = '';
+  var textura = new THREE.TextureLoader().load('http://francisgoba.github.io/rv/bb8body.jpg');
   this.cuerpos=new THREE.Mesh(new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56), new THREE.MeshPhongMaterial({color:0xffffff}));
-  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({color:0xffffff}));
-  this.cuerpos.position.y=8;
-  this.cuerpoi.position.y=-5.5;
-  this.cuerpos.position.x=0;
-  this.cuerpoi.position.x=0;
-  this.add(this.cuerpos)
-  this.add(this.cuerpoi)
-}
-
-
-function Cabina(){
-  THREE.Object3D.call(this);
+  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({map:textura}));
   this.cabina=new THREE.Mesh(new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI),new THREE.MeshPhongMaterial({color:0xffffff}));
   this.antena=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cuerpos.position.y=8;
+  this.cuerpoi.position.y=-5.5;
   this.cabina.position.y=2.35;
   this.antena.position.y=3.85;
+  this.add(this.cuerpos)
+  this.add(this.cuerpoi)
   this.add(this.cabina);
   this.add(this.antena);
 }
 
-Casco.prototype=new THREE.Object3D();
-Cabina.prototype=new THREE.Object3D();
+Ovni.prototype=new THREE.Object3D();
 
 function setup(){
-  cascoOvni = new Casco();
-  cabinaOvni = new Cabina();  
+  Ovnibot = new Ovni();
   luzPuntual = new THREE.PointLight(0xffffff);
   luzPuntual.position.x=10;
   luzPuntual.position.y=-7;
   luzPuntual.position.z=40;
   escena = new THREE.Scene();
-  escena.add(cascoOvni);
-  escena.add(cabinaOvni);
+  escena.add(Ovnibot);
   escena.add(luzPuntual);
   camara = new THREE.PerspectiveCamera();
   camara.position.z=12;
@@ -45,7 +37,7 @@ function setup(){
 function loop(){
   requestAnimationFrame( loop );
   renderer.render( escena, camara);
-  cascoOvni.rotation.y+=0.01;
+  Ovnibot.rotation.y+=0.01;
 }
 
 var escena,camara,luzPuntual,renderer;
