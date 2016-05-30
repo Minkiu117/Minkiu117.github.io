45f1e21b59f26cbc8bc168b08a8814a5afacb9a0
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 05:03:29 -0500

    Update robotcolisiones.js

--- rv/robotcolisiones.js
+++ rv/robotcolisiones.js
@@ -1,44 +1,40 @@
-function BB8Cabeza(){
- THREE.Object3D.call(this);
- THREE.ImageUtils.crossOrigin = '';
- var texturahead = THREE.ImageUtils.loadTexture('http://francisgoba.github.io/rv/bb8head2.jpg');
- this.cabeza=new THREE.Mesh(new THREE.SphereGeometry    (1.15,100,50,0,Math.PI*2,3*Math.PI/2,Math.PI),new THREE.MeshPhongMaterial({map:texturahead}));
- this.cuello=new THREE.Mesh(new THREE.CylinderGeometry(1.15,1,0.2,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.antena1=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.antena2=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.5,100),new     THREE.MeshPhongMaterial({color:0xffffff}));
- this.cabeza.position.y=1.95;
- this.antena1.position.y=3.35;
- this.antena1.position.x=0.13;
- this.antena2.position.y=3.1;
- this.antena2.position.x=-0.15;
- this.cuello.position.y=1.85;
- this.add(this.cabeza);
- this.add(this.antena1);
- this.add(this.antena2);
- this.add(this.cuello);
+function Cabina(){
+  THREE.Object3D.call(this);
+  THREE.ImageUtils.crossOrigin = '';
+  var texturacab = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/cab.jpg');
+  this.cabina=new THREE.Mesh(new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI),new THREE.MeshPhongMaterial({map:texturacab}));
+  this.antena=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cabina.position.y=2.35;
+  this.antena.position.y=3.85;
+  this.add(this.cabina);
+  this.add(this.antena);
 }
 
-BB8Cabeza.prototype = new THREE.Object3D();
+Cabina.prototype=new THREE.Object3D();
 
-function BB8(){
- THREE.Object3D.call(this);
- THREE.ImageUtils.crossOrigin = '';
- var textura = THREE.ImageUtils.loadTexture('http://francisgoba.github.io/rv/bb8body.jpg');
- this.cuerpo=new THREE.Mesh(new THREE.SphereGeometry(2,100,100), new THREE.MeshPhongMaterial ({map:textura}));
- this.cabezabb8 = new BB8Cabeza();
- this.cuerpo.rotation.z=-0.25;  
-
-  this.add(this.cuerpo);
-  this.add(this.cabezabb8);
+function Ovni(){
+  THREE.Object3D.call(this);
+  THREE.ImageUtils.crossOrigin = '';
+  var texturasup = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/sup.jpg');
+  var texturainf = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/inf.jpg');
+  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56), new THREE.MeshPhongMaterial({map:texturainf}));
+  this.cuerpos=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({map:texturasub}));
+  this.cuerpoi.position.y=8;
+  this.cuerpos.position.y=-5.5;
+  this.cabinaovni = new Cabina();
+  this.cuerpoi.rotation.y=-0.25;
+  this.cuerpos.rotation.y=-0.25;
+  this.add(this.cuerpos)
+  this.add(this.cuerpoi)
+  this.add(this.cabinaovni);
 }
 
-BB8.prototype=new THREE.Object3D();
+Ovni.prototype=new THREE.Object3D();
 
 function setup(){
 
- BB8modelo = new BB8();
- BB8modelo.rotation.x=Math.PI/2; 	
- //BB8modelocabeza.rotation.x=1;
+ Ovnibot = new Casco();
+ Ovnibot.rotation.x=Math.PI/2; 	
  luzPuntual = new THREE.PointLight(0xffffff);
  luzPuntual.position.x=0;  
  luzPuntual.position.y=10;
@@ -58,21 +54,21 @@ function setup(){
  camara = new THREE.PerspectiveCamera();
  camara.position.z=40;
  
- raycaster1 = new THREE.Raycaster(BB8modelo.position,new THREE.Vector3(1,0,0));
- raycaster2 = new THREE.Raycaster(BB8modelo.position,new THREE.Vector3(-1,0,0));
- raycaster3 = new THREE.Raycaster(BB8modelo.position,new THREE.Vector3(0,1,0));
- raycaster4 = new THREE.Raycaster(BB8modelo.position,new THREE.Vector3(0,-1,0));
+ raycaster1 = new THREE.Raycaster(Ovnibot.position,new THREE.Vector3(1,0,0));
+ raycaster2 = new THREE.Raycaster(Ovnibot.position,new THREE.Vector3(-1,0,0));
+ raycaster3 = new THREE.Raycaster(Ovnibot.position,new THREE.Vector3(0,1,0));
+ raycaster4 = new THREE.Raycaster(Ovnibot.position,new THREE.Vector3(0,-1,0));
 
  escena.add(cubo);
  escena.add(cubo2);
  escena.add(cubo3);
  escena.add(cubo4);
  escena.add(camara);
- escena.add(BB8modelo);
+ escena.add(Ovnibot);
  escena.add(luzPuntual);
  step=0.1;
  step2=step*2;
- cabezarota=Math.PI+0.1;	
+ cabinarota=Math.PI+0.1;	
  renderer = new THREE.WebGLRenderer();
  renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
  document.body.appendChild(renderer.domElement);
@@ -92,30 +88,34 @@ function loop(){
      (obstaculo4.length > 0 && (obstaculo4[0].distance<=2)))
  {
   angulo+=Math.PI/2;
-  cabezarota=Math.PI/4+0.8;
+  cabinarota=Math.PI/4+0.8;
  }
 
- BB8modelo.position.x+=Math.cos(angulo)*step;
- BB8modelo.position.y+=Math.sin(angulo)*step;
- if(cabezarota<Math.PI)
+ Ovnibot.position.x+=Math.cos(angulo)*step;
+ Ovnibot.position.y+=Math.sin(angulo)*step;
+ if(cabinarota<Math.PI)
  {
-  cabezarota+=0.05;
+  cabinarota+=0.05;
  }
- if(cabezarota==Math.PI)
+ if(cabinarota==Math.PI)
  {
-  inc=cabezarota+angulo+0.4;
-  cabezarota=0;
+  inc=cabinarota+angulo+0.4;
+  cabinarota=0;
  }
- BB8modelo.cabezabb8.rotation.y=angulo+0.4+cabezarota;
+ Ovnibot.cabinaovni.rotation.y=angulo+0.4+cabinarota;
  if(angulo%Math.PI/2!=0){
- BB8modelo.cuerpo.rotation.x+=step2;}
+ Ovnibot.cuerpos.rotation.x+=step2;
+ Ovnibot.cuerpoi.rotation.x+=step2;
+ }
  else{
- BB8modelo.cuerpo.rotation.y+=step2;}
+ Ovnibot.cuerpos.rotation.y+=step2;
+ Ovnibot.cuerpoi.rotation.y+=step2;
+ }
  
- raycaster1.set(BB8modelo.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
- raycaster2.set(BB8modelo.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
- raycaster3.set(BB8modelo.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
- raycaster4.set(BB8modelo.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));	
+ raycaster1.set(Ovnibot.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
+ raycaster2.set(Ovnibot.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
+ raycaster3.set(Ovnibot.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));
+ raycaster4.set(Ovnibot.position, new THREE.Vector3(Math.cos(angulo),Math.sin(angulo),0));	
 
  renderer.render(escena,camara);
  requestAnimationFrame(loop);
