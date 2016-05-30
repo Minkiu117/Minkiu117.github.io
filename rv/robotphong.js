8134d5dd982275c262d6412cc2ba95b231884dcb
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 01:48:38 -0500

    Create robotphong.js

---
+++ rv/robotphong.js
@@ -0,0 +1,54 @@
+function Casco(){
+  THREE.Object3D.call(this);
+  this.cuerpos=new THREE.Mesh(new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56), new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cuerpos.position.y=8;
+  this.cuerpoi.position.y=-5.5;
+  this.cuerpos.position.x=0;
+  this.cuerpoi.position.x=0;
+  this.add(this.cuerpos)
+  this.add(this.cuerpoi)
+}
+
+
+function Cabina(){
+  THREE.Object3D.call(this);
+  this.cabina=new THREE.Mesh(new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI),new THREE.MeshPhongMaterial({color:0x888888}));
+  this.antena=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cabina.position.y=2.35;
+  this.antena.position.y=3.85;
+  this.add(this.cabina);
+  this.add(this.antena);
+}
+
+Casco.prototype=new THREE.Object3D();
+Cabina.prototype=new THREE.Object3D();
+
+function setup(){
+  cascoOvni = new Casco();
+  cabinaOvni = new Cabina();  
+  luzPuntual = new THREE.PointLight(0xffffff);
+  luzPuntual.position.x=10;
+  luzPuntual.position.y=10;
+  luzPuntual.position.z=10;
+  escena = new THREE.Scene();
+  escena.add(cascoOvni);
+  escena.add(cabinaOvni);
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z=12;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize (window.innerHeight*0.95, window.innerHeight*0.95);
+  document.body.appendChild( renderer.domElement );
+}
+
+function loop(){
+  requestAnimationFrame( loop );
+  renderer.render( escena, camara);
+  cabinaOvni.rotation.y+=0.01;
+}
+
+var escena,camara,renderer;
+var cabinaOvni,cascoOvni;
+
+setup();
+loop();
