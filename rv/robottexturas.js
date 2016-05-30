3f5ea14075910884e277b62dde87132351b7d970
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 03:42:58 -0500

    Update robottexturas.js

--- rv/robottexturas.js
+++ rv/robottexturas.js
@@ -1,13 +1,13 @@
 function Ovni(){
   THREE.Object3D.call(this);
   THREE.ImageUtils.crossOrigin = '';
-  var textura = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/inf.jpg');
-  this.cuerpos=new THREE.Mesh(new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56), new THREE.MeshPhongMaterial({color:0xffffff}));
-  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({map:textura}));
+  var textura = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/sup.jpg');
+  this.cuerpoi=new THREE.Mesh(new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56), new THREE.MeshPhongMaterial({color:0xffffff}));
+  this.cuerpos=new THREE.Mesh(new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68), new THREE.MeshPhongMaterial({map:textura}));
   this.cabina=new THREE.Mesh(new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI),new THREE.MeshPhongMaterial({color:0xffffff}));
   this.antena=new THREE.Mesh(new THREE.CylinderGeometry(0.02,0.02,0.8,100),new THREE.MeshPhongMaterial({color:0xffffff}));
-  this.cuerpos.position.y=8;
-  this.cuerpoi.position.y=-5.5;
+  this.cuerpoi.position.y=8;
+  this.cuerpos.position.y=-5.5;
   this.cabina.position.y=2.35;
   this.antena.position.y=3.85;
   this.add(this.cuerpos)
