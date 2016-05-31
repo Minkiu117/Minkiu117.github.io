9131ae06f5123279c88af2e90f93e0d5ab194fc5
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-30 23:22:31 -0500

    Update robotparedes.js

--- rv/robotparedes.js
+++ rv/robotparedes.js
@@ -90,9 +90,8 @@ Environment.prototype.setMap=function(map){
 Ovni.prototype.sense=function(environment){
  this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
  var obstaculo = this.sensor.intersectObjects(environment.children,true);
- if ((obstaculo.length>0&&(obstaculo[0].distance<=2))){
+ if ((obstaculo.length>0&&(obstaculo[0].distance<=2.2))){
   this.sensor.colision=true;
-  //obstaculo[0].object.material.color.set(0xff0000);
   obstaculo[0].object.material=new THREE.MeshBasicMaterial({color:0xffff00});}
  else
   this.sensor.colision=false;
