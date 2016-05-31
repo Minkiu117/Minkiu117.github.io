9ee5989189a895eb1aefece6c41d3a2df91a3acf
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-31 12:24:56 -0500

    Update labyrinth.js

--- rv/labyrinth.js
+++ rv/labyrinth.js
@@ -183,12 +183,12 @@ function setup(){
 
  entorno=new Environment();
  entorno.setMap(mapa);
- var floor=new THREE.Mesh(new THREE.BoxGeometry(28,30,0.1), new THREE.MeshLambertMaterial({color:0x00ff00}));
+ var floor=new THREE.Mesh(new THREE.BoxGeometry(34,36,0.1), new THREE.MeshLambertMaterial({color:0x00ff00}));
  floor.position.z=-1.5;
  floor.position.x=-3.5;
  floor.position.y=0.5;
  iluminacion = new THREE.PointLight(0xffffff);
- iluminacion.position.z=20;
+ iluminacion.position.z=0;
  iluminacion.position.y=10;
  
  camara=new THREE.PerspectiveCamera();
