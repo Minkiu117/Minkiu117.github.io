f50f027aecd76909ac44a4b196417041cb8c9e52
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-31 13:03:11 -0500

    Update labyrinth.js

--- rv/labyrinth.js
+++ rv/labyrinth.js
@@ -184,10 +184,10 @@ function setup(){
  entorno=new Environment();
  entorno.setMap(mapa);
  THREE.ImageUtils.crossOrigin = '';
- var texturap = THREE.ImageUtils.loadTexture('texturas/ecopiedra.jpg');   //Cargo la textura de las paredes
+ var texturap = THREE.ImageUtils.loadTexture('texturas/piso2.jpg');   //Cargo la textura de las paredes
  texturap.wrapS = texturap.wrapT = THREE.RepeatWrapping; 	//Defino que la imagen se repita a lo largo de la malla 
  texturap.anisotropy=256;										//resalta el detalle de la textura
- var floor=new THREE.Mesh(new THREE.BoxGeometry(28,34,0.1), new THREE.MeshLambertMaterial({map:texturap}));//Creo el material de la pared tipo Lambert con la textura dada
+ var floor=new THREE.Mesh(new THREE.BoxGeometry(34,36,0.1), new THREE.MeshLambertMaterial({map:texturap}));//Creo el material de la pared tipo Lambert con la textura dada
  floor.position.z=-0.5;
  floor.position.x=-1.5;
  floor.position.y=0.5;
