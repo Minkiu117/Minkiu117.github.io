1b9c566cf96f89127521b498c9b324db0bc8205d
Minkiu117 <cuttrio_c6@hotmail.com>
2016-06-01 05:21:53 -0500

    Create robotperspective.js

---
+++ rv/robotperspective.js
@@ -0,0 +1,72 @@
+var keyboard = new THREEx.KeyboardState();
+function setup(){
+function setup() {
+  
+  var esferaFormai = new THREE.SphereGeometry(7, 200, 200, 0, Math.PI*2, Math.PI, .56);
+  var esferaFormas = new THREE.SphereGeometry(8, 200, 200, 0, Math.PI*2, 0, .68);
+  var mediaesferaForma = new THREE.SphereGeometry( 1.1, 100, 100, 0, Math.PI*2, 3*Math.PI/2, Math.PI);
+  var cilindroForma = new THREE.CylinderGeometry(0.02,0.02,0.8,100);
+  
+  var esfera1 = new THREE.Mesh(esferaFormas);
+  var esfera2 = new THREE.Mesh(esferaFormai);
+  var mediaesfera1 = new THREE.Mesh(mediaesferaForma);
+  var cilindromalla = new THREE.Mesh( cilindroForma);
+  
+  cilindromalla.position.y=3.85;
+  cilindromalla.position.x=0;
+  mediaesfera1.position.y=2.35;
+  esfera2.position.y=8;
+  esfera1.position.y=-5.5;
+
+  var forma = new THREE.Geometry();
+  
+  THREE.GeometryUtils.merge(forma, esfera1);
+  THREE.GeometryUtils.merge(forma, esfera2);
+  THREE.GeometryUtils.merge(forma, mediaesfera1);
+  THREE.GeometryUtils.merge(forma, cilindromalla);
+  
+  malla = new THREE.Mesh( forma );
+  
+  escena = new THREE.Scene();
+  escena.add(malla);
+  camara=new THREE.PerspectiveCamera();
+camara.position.z=30;
+
+
+
+camara2 = new THREE.OrthographicCamera( 16 / - 2, 16 / 2, 10 / 2, 10 / - 2, 1, 1000 );
+camara2.position.z=15;
+
+
+camara3 = new THREE.PerspectiveCamera( 30, 10 / 16, 1, 1000 );
+camara3.position.z=60;
+camara3.position.x=5;
+camara3.position.y=5;
+escena.add(camara);
+escena.add(camara2);
+escena.add(camara3);
+
+renderer=new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+
+
+
+function loop(){
+
+
+if (keyboard.pressed("P")) {
+renderer.render(escena,camara3);
+}
+else
+{
+renderer.render(escena,camara2);
+}
+requestAnimationFrame(loop);
+
+}
+
+var camara,camara2,camara3,escena,renderer,malla,mallat;
+setup();
+loop();
