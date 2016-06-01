111746f23bd5b3e70982e75b9e25d82264a09a50
Minkiu117 <cuttrio_c6@hotmail.com>
2016-05-31 22:29:20 -0500

    Update labyrinth.js

--- rv/labyrinth.js
+++ rv/labyrinth.js
@@ -109,7 +109,7 @@ Environment.prototype.setMap=function(map){
 Ovni.prototype.sense=function(environment){
  this.sensor.set(this.position, new THREE.Vector3(Math.PI/2+Math.cos(this.rotation.z),Math.PI/2+Math.sin(this.rotation.z),0));
  var obstaculo = this.sensor.intersectObjects(environment.children,true);
- if ((obstaculo.length>0&&(obstaculo[0].distance<=2.2))){
+ if ((obstaculo.length>0&&(obstaculo[0].distance<=5.2))){
   this.sensor.colision=true;
  THREE.ImageUtils.crossOrigin = '';
  var texturawm = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/magma.jpg'); 
