32991282c2ebe53b232c294cc5384c77507cee3f
Minkiu117 <cuttrio_c6@hotmail.com>
2016-11-16 22:43:45 -0600

    Update Espaciodeprueba.js

--- rv/Espaciodeprueba.js
+++ rv/Espaciodeprueba.js
@@ -278,11 +278,11 @@ function setup(){
  entorno=new Environment();
  entorno.setMap(mapa);
  THREE.ImageUtils.crossOrigin = '';
- var texturap = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/pisob.jpg');   //Cargo la textura de las paredes
+ var texturap = new THREE.TextureLoader().load('http://minkiu117.github.io/rv/pisob.jpg');   //Cargo la textura del piso
  texturap.wrapS = texturap.wrapT = THREE.RepeatWrapping; 	//Defino que la imagen se repita a lo largo de la malla
  texturap.repeat.set( 10, 10 );
  texturap.anisotropy=256;										//resalta el detalle de la textura
- var floor=new THREE.Mesh(new THREE.BoxGeometry(34,20,0.1), new THREE.MeshLambertMaterial({map:texturap}));//Creo el material de la pared tipo Lambert con la textura dada
+ var floor=new THREE.Mesh(new THREE.BoxGeometry(25,36,0.1), new THREE.MeshLambertMaterial({map:texturap}));//Creo el material de la pared tipo Lambert con la textura dada
  floor.position.z=-0.5;
  floor.position.x=-1.5;
  floor.position.y=0.5;
